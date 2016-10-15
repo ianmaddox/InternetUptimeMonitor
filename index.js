4128b7a3c5ecc35e6eb2365a18a72333716fc096
Jay Taylor <outtatime@gmail.com>
2016-10-15 13:02:56 -0700

    Fixed scoping for const declarations.
    Adjusted program to ensure const declarations are referencable when needed.
    
    Fixes `ReferenceError: PING_INTERVAL is not defined` and other similar
    errors.

--- index.js
+++ index.js
@@ -11,37 +11,38 @@ try {
   const API_KEY = conf.apiKey;
   const PING_INTERVAL = conf.checkFrequencySeconds;
   const PUSH_FREQ = conf.publishFrequencySeconds;
+
+    console.log("Starting internet connection monitor.\nChecking every " + PING_INTERVAL + " seconds.\nUploading that data to thingspeak.com every " + PUSH_FREQ + " seconds.");
+
+    const URL = 'https://api.thingspeak.com/update?api_key={apikey}&created_at={stamp}&field1={isOnline}'
+
+    var ping = function() {isOnline(function(err, online) {
+        var check = {stamp: moment().tz('GMT').format(), online:online};
+        backlog.push(check);
+        if(online && backlog.length >= (PUSH_FREQ/PING_INTERVAL)) {
+            while(event = backlog.shift()) {
+                pushToAPI(event.stamp, event.online);
+            }
+        }
+    })};
+
+    function pushToAPI(stamp, status) {
+        var api = URL
+          .replace('{isOnline}', status+0)
+          .replace('{stamp}', encodeURIComponent(stamp))
+          .replace('{apikey}', API_KEY);
+        rest(api)
+        .then(o => {
+          if(o.status == 400) {
+            "Recieved a 400 error from the server. Check your thingspeak.com API key in conf.json";
+          }
+          console.log(stamp,status?'Online':'Offline',o.status);
+        });
+    }
+
+    scheduler().every(PING_INTERVAL).seconds().run(ping);
 } catch(e) {
   console.log("Please copy the file conf.json.default to conf.json. Place your thingspeak.com API key in the appropriate location within the file.");
   process.exit();
 }
 
-console.log("Starting internet connection monitor.\nChecking every " + PING_INTERVAL + " seconds.\nUploading that data to thingspeak.com every " + PUSH_FREQ + " seconds.");
-
-const URL = 'https://api.thingspeak.com/update?api_key={apikey}&created_at={stamp}&field1={isOnline}'
-
-var ping = function() {isOnline(function(err, online) {
-    var check = {stamp: moment().tz('GMT').format(), online:online};
-    backlog.push(check);
-    if(online && backlog.length >= (PUSH_FREQ/PING_INTERVAL)) {
-        while(event = backlog.shift()) {
-            pushToAPI(event.stamp, event.online);
-        }
-    }
-})};
-
-function pushToAPI(stamp, status) {
-    var api = URL
-      .replace('{isOnline}', status+0)
-      .replace('{stamp}', encodeURIComponent(stamp))
-      .replace('{apikey}', API_KEY);
-    rest(api)
-    .then(o => {
-      if(o.status == 400) {
-        "Recieved a 400 error from the server. Check your thingspeak.com API key in conf.json";
-      }
-      console.log(stamp,status?'Online':'Offline',o.status);
-    });
-}
-
-scheduler().every(PING_INTERVAL).seconds().run(ping);
